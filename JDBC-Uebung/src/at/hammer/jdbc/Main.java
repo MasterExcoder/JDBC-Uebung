6816fe593f3aef4ad57f0dc136cce45776bdc5da
Samuel Hammer <ham14447@spengergasse.at>
2013-12-03 10:37:36 +0100

    Successfully connected to database in localhost.

--- JDBC-Uebung/src/at/hammer/jdbc/Main.java
+++ JDBC-Uebung/src/at/hammer/jdbc/Main.java
@@ -1,9 +1,41 @@
 package at.hammer.jdbc;
 
-public class Main {
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.Statement;
 
+public class Main {
+	
 	public static void main(String[] args) {
 		
+		String username = "root";
+		String password = "";
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/?", username, password);
+			con.setAutoCommit(false);
+			Statement statement = con.createStatement();
+			
+			statement.executeQuery("use chinook");
+			
+			ResultSet rsl = statement.executeQuery("SELECT * FROM ARTIST");
+			while(rsl.next()) {
+				System.out.println(rsl.getString("name"));
+			}
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
