5570c5a5a0da7cc89f718e2af40fe673c1b4298c
Samuel Hammer <ham14447@spengergasse.at>
2013-12-03 15:50:55 +0100

    Code documentation

--- JDBC-Uebung/src/at/hammer/jdbc/Main.java
+++ JDBC-Uebung/src/at/hammer/jdbc/Main.java
@@ -14,13 +14,17 @@ public class Main {
 		String password = "";
 		
 		try {
+			//Einbinden des Oracle MySqlConnectors in java
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			//Aufbau der Verbindung zur Datenbank
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/?", username, password);
 			con.setAutoCommit(false);
-			Statement statement = con.createStatement();
 			
+			//Ausführen verschiedener Statements
+			Statement statement = con.createStatement();
 			statement.executeQuery("use chinook");
 			
+			//Ausgabe der Namen aller Künstler aus der Tabelle ARTIST
 			ResultSet rsl = statement.executeQuery("SELECT * FROM ARTIST");
 			while(rsl.next()) {
 				System.out.println(rsl.getString("name"));
